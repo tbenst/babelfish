035ee98bfa3baf57325de1aedde88090026b0ce3
tbenst <lensman@tylerbenster.com>
2019-12-22 13:12:11 -0800

    debug hydra CI

--- release.nix
+++ release.nix
@@ -1,15 +1,23 @@
+{ ... }:
+let
+  pkgs = (import <nixpkgs> {});
+in {
+  hello = pkgs.hello;
+}
+
 # this file is used for Continuous Integration
 # { supportedSystems ? [ "x86_64-linux" ]}:
-let
-  nixpkgsSHA = "07be0186f298e5b16897a168eae6ab01a5540fc4";
-  pkgs = import (fetchTarball
-    https://github.com/tbenst/nixpkgs/archive/${nixpkgsSHA}.tar.gz) {
-      system = builtins.currentSystem;
-      overlays = import ./overlays.nix;
-    };
+# let
+#   nixpkgsSHA = "07be0186f298e5b16897a168eae6ab01a5540fc4";
+#   pkgs = import (fetchTarball
+#     https://github.com/tbenst/nixpkgs/archive/${nixpkgsSHA}.tar.gz) {
+#       system = builtins.currentSystem;
+#       overlays = import ./overlays.nix;
+#     };
 
-  jobs = rec {
-    babelfish = pkgs.python3Package.babelfish;
-  };
-in
-  jobs
+#   jobs = rec {
+#     hello = pkgs.hello;
+#     # babelfish = pkgs.python3Package.babelfish;
+#   };
+# in
+#   jobs
