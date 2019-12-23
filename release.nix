353b416c1d1ff8607ed4f211230eaaf3e1612a34
Tyler Benster <tbenst@gmail.com>
2019-12-23 01:23:49 -0800

    debug hydra CI

--- release.nix
+++ release.nix
@@ -13,7 +13,7 @@ let
          ];
         allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
           "cudnn_cudatoolkit_10"
-          "cudatoolkit-10.1.243"
+          "cudatoolkit"
         ];
       };
     };
