689da01b4f22394132bf40d997217e361aa294cf
tbenst <lensman@tylerbenster.com>
2019-12-22 20:38:42 -0800

    debug hydra CI

--- release.nix
+++ release.nix
@@ -2,12 +2,11 @@
 # { supportedSystems ? [ "x86_64-linux" ]}:
 let
   nixpkgsSHA = "07be0186f298e5b16897a168eae6ab01a5540fc4";
-  pkgs = import (fetchTarball
+  pkgs = (import (fetchTarball
     https://github.com/tbenst/nixpkgs/archive/${nixpkgsSHA}.tar.gz) {
       system = builtins.currentSystem;
       overlays = import ./overlays.nix;
-    };
-
+    });
   jobs = rec {
     hello = pkgs.hello;
     babelfish = pkgs.python3Package.babelfish;
